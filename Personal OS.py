321b87933fb948d013ef039226d2c6184add70b6
Shivam Pachpute <74021700+Xlash9914-cyber@users.noreply.github.com>
2020-12-06 00:22:20 +0530

    Add files via upload

---
+++ Personal OS.py
@@ -0,0 +1,61 @@
+from tqdm import tqdm
+import time
+password = str(input("Password: ".title()))
+a = "awdslkj" in password
+
+
+
+
+if(a):
+    for item in tqdm(range(100)):
+        time.sleep(0.05)
+    print ("\n                                                                      TM")
+    print(" __   ___       ___ _____ _    ___  ___   ___  __ _  _   \n \ \ / / |     |  _/ ____| |  |_  |/ _ \ / _ \/_ | || |  \n  \ V /| | __ _| || (___ | |__  | | (_) | (_) || | || |_ \n   > < | |/ _` | | \___ \| '_ \ | |\__, |\__, || |__   _|\n  / . \| | (_| | | ____) | | | || |  / /   / / | |  | |  \n /_/ \_\_|\__,_| ||_____/|_| |_|| | /_/   /_/  |_|  |_|  \n               |___|          |___|                      \n                                                         \n      _____        _          ____                          \n     |  __ \      | |        |  _ \                         \n     | |  | | __ _| |_ __ _  | |_) | __ _ ___  ___          \n     | |  | |/ _` | __/ _` | |  _ < / _` / __|/ _ \         \n     | |__| | (_| | || (_| | | |_) | (_| \__ \  __/         \n     |_____/ \__,_|\__\__,_| |____/ \__,_|___/\___|         \n                                                            ")    
+    print ("DATA BASE ACCESSED")
+    time.sleep(2)
+    print ("IF YOU TYPE THE FOLLOWING: ")
+    time.sleep(1)
+    print ("unity          :- "+"this command opens unity hub".title())
+    time.sleep(1)
+    print ("mydata         :- "+"this command opens the main folder".title())
+    time.sleep(1)
+    print ("blender        :- "+"this command opens the blender software".title())
+    time.sleep(1)
+    print ("minecraft      :- "+"this command opens the minecraft".title())
+    time.sleep(1)
+    command = str(input("Command: "))
+    b = "mydata" in command
+    c = "unity" in command
+    d = "blender" in command
+    e = "minecraft" in command
+            
+    import os
+    if(b):
+        os.startfile("D:\Shivam")
+    if(c):
+        os.startfile('D:\\Shivam\\Software\\UnityHub.lnk')
+    if(d):
+        os.startfile("C:\\Program Files (x86)\\Blender Foundation\\Blender\\blender.exe")
+    if(e):
+        os.startfile("C:\\Users\\user\\AppData\\Roaming\\.minecraft\\TLauncher.exe")
+    if(b == c == d == e):
+        print("Syntax_Error: Wrong_Word_Typed")
+        time.sleep(1)
+        print("OS Will shut down in: T-0:03")
+        time.sleep(1)
+        print("OS Will shut down in: T-0:02")
+        time.sleep(1)
+        print("OS Will shut down in: T-0:01")
+        time.sleep(1)
+        print("OS Will shut down in: T-0:00")
+
+else:print("access denied".center(74,"=").upper())
+
+
+                                                                                            
+                                                                                            
+                                                                                            
+
+
+
+
